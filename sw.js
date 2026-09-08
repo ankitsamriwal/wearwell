83adab59acd23b40486008f9e6475d6f684094e4
Ankit Samriwal <ankitsamriwal@gmail.com>
2026-09-08 20:51:08 +0400

    WearWell: on-device wardrobe stylist - weather+occasion+time outfit picks, closet scan with photos, week planner, purchase history and wardrobe insights

---
+++ sw.js
@@ -0,0 +1,9 @@
+const C='wearwell-v1';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin===location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(n=>{const cl=n.clone();caches.open(C).then(c=>c.put(e.request,cl));return n})))}
+  else{e.respondWith(fetch(e.request).catch(()=>new Response('offline',{status:503})))}
+});
