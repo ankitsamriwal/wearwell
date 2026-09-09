4093fcf46f002b1566e52f4dcb445f859b83f57b
Ankit Samriwal <13885718+ankitsamriwal@users.noreply.github.com>
2026-09-09 09:48:42 +0400

    fix: closet header layout - compact bulk/add buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='akp-v3';
+const C='akp-v4';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
